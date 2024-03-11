5ae6a64ec5a5ad66bd54348b6cc072a427702820
mhjibon360 <137417891+mhjibon360@users.noreply.github.com>
2024-03-11 09:39:20 +0600

    Add files via upload

---
+++ jquery-form-validate.js
@@ -0,0 +1,29 @@
+<script type="text/javascript">
+        $(document).ready(function() {
+            $('#myForm').validate({
+                rules: {
+                    title: {
+                        required: true,
+                    },
+                    
+                },
+                messages: {
+                    title: {
+                        required: 'Please enter home title',
+                    },
+                    
+                },
+                errorElement: 'strong',
+                errorPlacement: function(error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function(element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                },
+            });
+        });
+    </script>
